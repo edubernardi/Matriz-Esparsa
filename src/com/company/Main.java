81f43717fefc71d233e968f17fff48ec538edbe5
edubernardi <77065000+edubernardi@users.noreply.github.com>
2021-03-29 10:41:13 -0300

    Changes to the test class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,29 +5,31 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	    MatrizEsparsa me = new MatrizEsparsa(10,10);
+    	int linhas = 30;
+    	int colunas = 3;
+	    MatrizEsparsa me = new MatrizEsparsa(linhas,colunas);
 
 	    int sLin, sCol;
 	    double sVal;
         Random r = new Random();
-	    for(int x = 0; x < 100; x++){
-	        sLin = r.nextInt(10);
-	        sCol = r.nextInt(10);
+	    for(int x = 0; x < (linhas * colunas * 0.01); x++){
+	        sLin = r.nextInt(linhas);
+	        sCol = r.nextInt(colunas);
 	        sVal = (double) r.nextInt(100000);
 	        me.set(sVal, sLin, sCol);
         }
 
 	    //testando set
-	    me.set(1,0,0);
-	    me.set(0,0,0);
+	    me.set(1,1,1);
 
 	    //testando armazena
 	    me.armazena("teste.txt");
 
 	    //testando copia
-	    double[][] matrix = me.copia(0,0,10,10);
-	    for (int i =0 ; i< 10; i++){
-	    	for (int j= 0; j < 10; j++){
+		System.out.println("Copiada:");
+	    double[][] matrix = me.copia(0,0, linhas, colunas);
+	    for (int i =0 ; i< matrix.length; i++){
+	    	for (int j= 0; j < matrix[i].length; j++){
 	    		System.out.print(matrix[i][j] + " ");
 			}
 	    	System.out.print("\n");
@@ -38,9 +40,10 @@ public class Main {
 		System.out.println("======================================");
 
 		//imprimindo transposta pra ver se funciona mesmo
-		matrix = t.copia(0,0,10,10);
-		for (int i =0 ; i< 10; i++){
-			for (int j= 0; j < 10; j++){
+		matrix = t.copia(0,0,colunas,linhas);
+		System.out.println("Transposta:");
+		for (int i =0 ; i< matrix.length; i++){
+			for (int j= 0; j < matrix[i].length; j++){
 				System.out.print(matrix[i][j] + " ");
 			}
 			System.out.print("\n");
@@ -48,10 +51,23 @@ public class Main {
 
 	    t.armazena("teste2.txt");
 		System.out.println("======================================");
+		//testando carrega
+		System.out.println("Carregada:");
 		me.carrega("teste.txt");
-		matrix = me.copia(0,0,10,10);
-		for (int i =0 ; i< 10; i++){
-			for (int j= 0; j < 10; j++){
+		matrix = me.copia(0,0,linhas,colunas);
+		for (int i =0 ; i< matrix.length; i++){
+			for (int j= 0; j < matrix[i].length; j++){
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+
+		System.out.println("======================================");
+		System.out.println("Transposta carregada:");
+		me.carrega("teste2.txt");
+		matrix = me.copia(0,0, colunas, linhas);
+		for (int i =0 ; i< matrix.length; i++){
+			for (int j= 0; j < matrix[i].length; j++){
 				System.out.print(matrix[i][j] + " ");
 			}
 			System.out.print("\n");
